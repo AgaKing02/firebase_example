8659b1570deb0e05b4c9b83659cd9e9f4537fcaf
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-02-16 11:26:32 +0600

    firebase example

---
+++ app/src/main/java/com/example/firebase_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.firebase_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, There!")
+    }
+}
