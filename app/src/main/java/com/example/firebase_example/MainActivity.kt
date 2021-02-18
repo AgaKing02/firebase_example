23e3257ca9b925aa3c42f75714e8285cc7ebcec6
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2021-02-18 22:58:00 +0600

    Write object

--- app/src/main/java/com/example/firebase_example/MainActivity.kt
+++ app/src/main/java/com/example/firebase_example/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.firebase_example
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +17,29 @@ class MainActivity : AppCompatActivity() {
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
-        myRef.setValue("Hello, There!")
+//        var James = Person("James", 30)
+        var employee=Employee("James Bond","Android developer?XD",
+            "123 South Street",51)
+        myRef.setValue(employee)
+
+
+//        myRef.setValue("Hello, From Android x Kotlin!")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.value as HashMap<String,Any>
+                Log.d("VALUE: ", value.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("ERROR: ", error.message)
+            }
+
+        })
     }
+
+    data class Employee(
+        var name: String, var position: String,
+        var homeAddress: String, var age: Int
+    )
 }
